ace42649ef474120a30c8acadcb152eab8e44af4
Juan José Moreno <juanmj05.dev@gmail.com>
2026-07-17 19:53:52 -0500

    fix(superadmin): resolve TS and NestJS DI errors

--- apps/api/src/core/realtime/redis-io.adapter.ts
+++ apps/api/src/core/realtime/redis-io.adapter.ts
@@ -7,7 +7,7 @@ import { buildCorsOptions } from '../../app.setup';
 import { ConfigService } from '@nestjs/config';
 
 export class RedisIoAdapter extends IoAdapter {
-  private adapterConstructor: ReturnType<typeof createAdapter>;
+  private adapterConstructor!: ReturnType<typeof createAdapter>;
 
   constructor(app: INestApplication, private readonly redis: RedisService, private readonly config: ConfigService) {
     super(app);
